f5d0dc954e64b871ec389364a5ac20e15e890960
Vansh Gandhi <vansh.vg18@gmail.com>
2023-07-24 17:10:05 +0530

    Integrated Mongodb

--- server/data/data.js
+++ server/data/data.js
@@ -1,1 +1,5 @@
+const mongoose = require("mongoose");
 
+const mongodb_url = process.env.MONGODB_URL; //Replace this with your mongodb url
+
+module.exports = mongoose.connect(mongodb_url);
--- server/modules/script.js
+++ server/modules/script.js
@@ -1,1 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("../data/data");
 
+const app = express();
+
+mongoose.connection.once("open", () => {
+  console.log("Mongodb is connected");
+});
+mongoose.connection.on("disconnected", () => {
+  console.log("Mongodb is disconnected");
+});
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+  console.log(`Server is listening on ${port}`);
+});
